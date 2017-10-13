b87b023bc3db345d9874bbe3fcfa129e3f9c3225
lmt <350056846@qq.com>
2017-10-13 17:25:02 +0800

    从服务端获取音频数据

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,5 +8,16 @@ for(var i= 0 ; i < list.length ; i++){
             list[j].className=''
         }
         this.className='selected'
+        load('/media/'+this.title);
     }
+}
+
+var xhr = new XMLHttpRequest();//创建一个ajax对象
+function load(url){
+    xhr.open('GET',url)//打开请求
+    xhr.responseType='arraybuffer'//数据类型，arraybuffer理解为服务器返回的音频数据以二进制数据形式
+    xhr.onload = function(){//请求成功后调用的事件处理程序
+        console.log(xhr.response)
+    }
+    xhr.send()//发送请求
 }
